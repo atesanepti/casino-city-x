25038196c238eff9b18653ec39b56f74abf98afb
san-bin-hoque <epti060@gmail.com>
2025-02-06 22:24:21 +0600

    v1

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+const nextConfig: NextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.mp3$/,
+      use: {
+        loader: "file-loader",
+      },
+    });
+    return config;
+  },
+};
+
+export default nextConfig;
---
+++ src/components/Logo.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Image from "next/image";
+import logo from "@/../public/assets/logo.png";
+
+const Logo = () => {
+  return (
+    <Image
+      src={logo}
+      alt="Casino Ciry 247"
+      className="w-32"
+      placeholder="blur"
+    />
+  );
+};
+
+export default Logo;
